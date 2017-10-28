15179ebf04f009ecfa72e71a49b0d7934e5fb7b2
eunjiruss <31670138+eunjiruss@users.noreply.github.com>
2017-10-27 20:22:17 -0700

    add

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,71 +9,105 @@ var crystalNum = 0;
 var crystalTotal = 0;
 var loss = 0;
 var win = 0;
- 
-
-//show a random number at the "start"; window on load randomNum="19-120"
-randomNum = Math.floor(Math.random() * (101 + 1)) + 19;
-$("#randomNum").html("Crystal To Harvest: " + randomNum);
+var seenAlready = [];
 
 //console.log(randomNum);
 
+function contains(elem, array) {
+	for (var i = 0; i < array.length; ++i) {
+		if (array[i] === elem) {
+			return true;
+		}
+	}
 
-//each of the 4 crystal will have a random number each round "1-12"
-//set a loop for 4 cryNum generator
-for (var i = 0; i < 4; i++) {
-	//console.log("Hello world");
-	var crystalNum = Math.floor(Math.random() * 12 - 1 + 1)+1;
-	//console.log(crystalNum);
-	var crystal = $("<div>");
-		crystal.attr({
-			"class": 'crystal',
-			"ComputerGuess": crystalNum
-		});
+	return false;
+}
 
-		crystal.html(crystalNum); //To show the number for test (checked)
+function setGameDefaults() {
+	randomNum = 0;
+	crystalNum = 0;
+	crystalTotal = 0;
+	seenAlready = [];
+}
 
-	$(".crystals").append(crystal);
-	//console.log("Crystal Test");
+function onGameReset() {
+	
+	// We're starting the game - set everything to default
+	setGameDefaults();
 
+	// Clear old crystals
+	$(".crystals").html("");
+	
+	//show a random number at the "start"; window on load randomNum="19-120"
+	randomNum = Math.floor(Math.random() * (101 + 1)) + 19;
+	$("#randomNum").html("Crystal To Harvest: " + randomNum);
+
+	//each of the 4 crystal will have a random number each round "1-12"
+	//set a loop for 4 cryNum generator
+	for (var i = 0; i < 4; i++) {
+		//console.log("Hello world");
+		var crystalNum = Math.floor(Math.random() * 12)+1;
+
+		while (true) {
+			if (!contains(crystalNum, seenAlready)) {
+				seenAlready.push(crystalNum);
+				break;
+			}
+
+			crystalNum = Math.floor(Math.random() * 12)+1;
+		}
+
+		console.log(crystalNum);
+		var crystal = $("<div>");
+			crystal.attr({
+				"class": 'crystal',
+				"ComputerGuess": crystalNum
+			});
+
+			crystal.html(crystalNum); //To show the number for test (checked)
+
+		$(".crystals").append(crystal);
+		//console.log("Crystal Test");
+	}
+}
+
+function setEventListeners() {
+	//on click, each crystal should have a random value between 0-12 (checked)
+	//These values should not show up "on.click"
+	$(".crystal").on('click', function () {
+		//console.log($(this).attr('ComputerGuess')); (checked)
+		//make each crystal a separate variable and then add the functions to add the values in//
+		var crystalValue = parseInt($(this).attr('ComputerGuess'));
+		crystalTotal += crystalValue;
+		console.log(crystalTotal); 
+	//But the incrementation of the values should show up upon each crystal "on.click" (checked)
+		
+		//if total crystal added is over the randomNum, then add point to "losses"
+		if(crystalTotal > randomNum) {
+			loss++;
+			alert("Try Again!");
+			$("#losses").html("Losses: " + loss);
+			onGameReset();
+
+		}
+
+
+		//If the total crystal added is equal to the randomNum, then add point to "wins" and alert "You Got It"
+		else if(crystalTotal === randomNum) {
+			win++;
+			alert("You Got It!");
+			$("#wins").html("Wins: " + win);
+		//console.log("You Got It!"); (checked)
+			alert("You Got It!");
+
+		}
+	});
 }
 
 
-
-//on click, each crystal should have a random value between 0-12 (checked)
-//These values should not show up "on.click"
-$(".crystal").on('click', function () {
-	//console.log($(this).attr('ComputerGuess')); (checked)
-	//make each crystal a separate variable and then add the functions to add the values in//
-	var crystalValue = parseInt($(this).attr('ComputerGuess'));
-	crystalTotal += crystalValue;
-	console.log(crystalTotal); 
-//But the incrementation of the values should show up upon each crystal "on.click" (checked)
-	
-	//if total crystal added is over the randomNum, then add point to "losses"
-	if(crystalTotal > randomNum) {
-		loss++;
-		alert("Try Again!");
-		$("#losses").html("Losses: " + loss);
-
-
-	}
-
-
-	//If the total crystal added is equal to the randomNum, then add point to "wins" and alert "You Got It"
-	else if(crystalTotal === randomNum) {
-		win++;
-		alert("You Got It!");
-		$("#wins").html("Wins: " + win);
-	//console.log("You Got It!"); (checked)
-		alert("You Got It!");
-
-	}
-
-
-
-
+$( document ).ready(function() {
+	onGameReset();
+	setEventListeners();
 });
-
-
 
 
